638e45c100e7003059de5076766c9ae6445c604c
Mosakowski <kubamosakowski@gmail.com>
2025-10-31 17:23:22 +0100

    pare zadan

--- blog/posts/admin.py
+++ blog/posts/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
-from .models import Category, Topic
+from .models import Category, Topic, Post
 
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(Topic)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name','description')
+    search_fields = ('name',)
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('name','category','created')
+    search_fields = ('name',)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('title',)}
+    readonly_fields = ('created_at',)
+    list_display = ('title','topic','text','slug','created_at','created_by','updated_at')
+    list_filter = ('topic__name', 'topic__category__name')
+
+    @admin.display(description='Topic')
+    def formatted_topic(self,obj):
+        return f"{obj.topic.name} ({obj.topic.category.name})"
--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,3 +1,5 @@
+import django.contrib.auth.models
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -7,8 +9,38 @@ class Category(models.Model):
     name = models.CharField(max_length=60)
     description = models.TextField(default="brak")
 
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
 
 class Topic(models.Model):
     name = models.CharField(max_length=60)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    text = models.TextField()
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    slug = models.SlugField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(django.contrib.auth.models.User, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('-created_at',)
+
+    def __str__(self):
+        words = self.text.split()
+        if len(words) > 5:
+            return " ".join(words[:5]) + "..."
+        return self.text
